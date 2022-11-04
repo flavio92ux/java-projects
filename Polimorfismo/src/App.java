ccf8aafa2ca0ea3cfe078747db2a7ab452cc650b
Flavio F <flavio.win@gmail.com>
2022-11-04 17:30:06 -0300

    Polimorfismo

---
+++ Polimorfismo/src/App.java
@@ -0,0 +1,19 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pessoa p1 = new Pessoa("Pedro da Silva", 1987, "PedroSilva@terra.com.br");
+
+        // System.out.println(p1.getNome());
+
+        Aluno a1 = new Aluno("Vanessa da Silva",1992, "123456", "vanessinha@vilabol.uol.com.br");
+
+        // System.out.println(a1.getRa());
+
+        Professor pr1 = new Professor("Pedro da Silva", 1983, "1233456", "giraffales@ig.com.br");
+
+        System.out.println(pr1.getMatricula());
+
+        System.out.println(p1 + "\n");
+        System.out.println(a1 + "\n");
+        System.out.println(pr1);
+    }
+}
---
+++ Polimorfismo/src/Televisao.java
@@ -0,0 +1,12 @@
+// Polimorfismo
+
+/* 
+ * - Poli - Muitos
+ * - Morfismo - formas
+ * 
+ * Objeto do mesmo tipo podem agir de formas diferentes
+ */
+
+public class Televisao {
+  
+}
