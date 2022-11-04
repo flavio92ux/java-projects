478e536fece19ff42dfc4ad578001f399d030be2
Flavio F <flavio.win@gmail.com>
2022-11-04 12:44:19 -0300

    Primeiro projeto - Herança

---
+++ Heranca/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Pessoa p1 = new Pessoa("Pedro da Silva", 1987);
+
+        System.out.println(p1.getNome());
+    }
+}
