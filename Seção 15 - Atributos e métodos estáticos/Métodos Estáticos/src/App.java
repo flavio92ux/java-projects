d8483904f5ca8ebdf0d217c78968391e8ed2af51
Flavio F <flavio.win@gmail.com>
2022-11-09 09:26:42 -0300

    Métodos estáticos

--- Seção 15 - Atributos e métodos estáticos/Métodos Estáticos/src/App.java
+++ Seção 15 - Atributos e métodos estáticos/Métodos Estáticos/src/App.java
@@ -1,28 +1,19 @@
-// Atributos estáticos
+// Métodos estáticos
 
-/* 
- * Atributos estáticos são atributos em que os valores são compartilhados
- * entre as instâncias da classe.
+/* Um método estático não depende de uma instância da classe para
+ * ser utilizado.
  * 
- * Para utilizar um atributo estático colocamos o nome da classe junto
- * ao atributo.
+ * Pode der utilizado conforme:
  * 
- * Exemplo:
- * NomeDaClasse.atributo
+ * NomeDaClasse.metodo();
  */
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Conta c1 = new Conta("Angelina Jolie");
+        Conta c1 = new Conta("Meu cliente");
         System.out.println(c1.getNumero());
         System.out.println(c1.getCliente());
 
-        Conta c2 = new Conta("Felicity Jones");
-        System.out.println(c2.getNumero());
-        System.out.println(c2.getCliente());
-
-        Conta c3 = new Conta("Maria da Graça Xuxa Meneguel");
-        System.out.println(c3.getNumero());
-        System.out.println(c3.getCliente());        
+        System.out.println("A próxima conta será: " + Conta.proximaConta());
     }
 }
--- Seção 15 - Atributos e métodos estáticos/Métodos Estáticos/src/Conta.java
+++ Seção 15 - Atributos e métodos estáticos/Métodos Estáticos/src/Conta.java
@@ -2,7 +2,7 @@ public class Conta {
   private int numero;
   private String cliente;
 
-  public static int contador = 1;
+  private static int contador = 1;
 
   public Conta(String cliente) {
     this.numero = contador;
@@ -17,5 +17,9 @@ public class Conta {
 
   public String getCliente() {
     return this.cliente;
+  }
+
+  public static int proximaConta() {
+    return Conta.contador;
   }
 }
